555cd5c10c239b856243776193544339f1dee50b
Ochudniak <ochudniak41@gmail.com>
2026-05-15 21:27:00 +0200

    Reapply "Poprawa Ramana 14.05"
    This reverts commit f4f7babfc4851c106119b07110b16e0bd4325f71.

--- backend/auction_system/users/views.py
+++ backend/auction_system/users/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserSerializer
+from django.shortcuts import get_object_or_404
+from .models import User
 
 
 class UserCreateView(APIView):
@@ -11,3 +13,12 @@ class UserCreateView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class UserCreateViewById(APIView)
+    def get(self, request, user_id):
+
