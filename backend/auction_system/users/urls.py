260a22372118320404312c780d4db729ee066302
Ochudniak <ochudniak41@gmail.com>
2026-05-16 15:33:11 +0200

    3.1 Issue

--- backend/auction_system/users/urls.py
+++ backend/auction_system/users/urls.py
@@ -3,5 +3,5 @@ from .views import UserCreateView, UserCreateViewById
 
 urlpatterns = [
     path("users/", UserCreateView.as_view()),
-    path("users/int:user_id>", UserCreateViewById.as_view())
+    path("users/<int:user_id>/", UserCreateViewById.as_view())
 ]
--- backend/auction_system/users/views.py
+++ backend/auction_system/users/views.py
@@ -19,16 +19,27 @@ class UserCreateView(APIView):
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-class UserCreateViewById(APIView)
+class UserCreateViewById(APIView):
     def get(self, request, user_id):
-        user = get_object_or_404(User, id=user_id)
+        user = get_object_or_404(User, id=user_id) 
         serializer = UserSerializer(user)
-        if serializer:
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, user_id):
         user = get_object_or_404(User, id=user_id)
-        serializer = UserSerializer(user)
+        serializer = UserSerializer(user, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, user_id):
+        user = get_object_or_404(User, id=user_id)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    
         
 
