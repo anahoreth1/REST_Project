f4825bee6800ec4f419a680846aed8237b0fa222
Ochudniak <ochudniak41@gmail.com>
2026-05-18 23:36:41 +0200

    test auctions

--- backend/auction_system/auctions/urls.py
+++ backend/auction_system/auctions/urls.py
@@ -4,5 +4,5 @@ from .views import AuctionListCreateView, AuctionDetailView, AuctionBiddingView
 urlpatterns = [
     path("auctions/", AuctionListCreateView.as_view(), name="auction-list-create"),
     path("auctions/<int:pk>/", AuctionDetailView.as_view(), name="auction-detail"),
-    path("auctions/<int:auction_id>/bids", AuctionBiddingView.as_view(), name="auction-bid")
+    path("auctions/<int:auction_id>/bids/", AuctionBiddingView.as_view(), name="auction-bid")
 ]
