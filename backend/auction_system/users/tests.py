9f8ba9591d21aefca27c4ebd4e8cdb7c3aee5ac6
Ochudniak <ochudniak41@gmail.com>
2026-06-17 19:23:35 +0200

    Update user tests

--- backend/auction_system/users/tests.py
+++ backend/auction_system/users/tests.py
@@ -16,10 +16,34 @@ class UserCreateViewTest(APITestCase):
         response = self.client.post("/api/users/", data, format="json")
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.filter(email="test@example.com").count(), 1)
+
+        user = User.objects.get(email="test@example.com")
+
+        self.assertEqual(user.email, "test@example.com")
+        self.assertEqual(user.name, "testuser")
+
+        self.assertNotIn("password", response.data)
+
 
     def test_create_user_without_name(self):
 
-        data = {"name": "", "email": "test@example.com"}
+        data = {"name": "", "email": "test@example.com", "password": "Password123"}
+
+        response = self.client.post("/api/users/", data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    
+    def test_create_user_without_password(self):
+        data = {"name": "Name", "email": "test@example.com", "password": ""}
+
+        response = self.client.post("/api/users/", data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_user_without_email(self):
+        
+        data = {"name": "", "email": "", "password": "Password123"}
 
         response = self.client.post("/api/users/", data, format="json")
 
@@ -27,12 +51,28 @@ class UserCreateViewTest(APITestCase):
 
     def test_create_user_with_wrong_email(self):
 
-        data = {"name": "testuser", "email": "not-an-email"}
+        data = {"name": "testuser", "email": "not-an-email", "password": "Password123"}
 
         response = self.client.post("/api/users/", data, format="json")
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
+    def test_create_user_with_duplicated_email(self):
+        User.objects.create(
+        name="firstuser",
+        email="test@example.com",
+        password="Password123",
+    )
+
+        data = {
+            "name": "seconduser", "email": "test@example.com", "password": "AnotherPassword123",
+        }
+
+        response = self.client.post("/api/users/", data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.filter(email="test@example.com").count(), 1)
+    
 
 class UserViewDetailTest(APITestCase):
     def setUp(self):
@@ -49,6 +89,11 @@ class UserViewDetailTest(APITestCase):
 
     def test_find_user_wrong_id_format(self):
         response = self.client.get("/api/users/abc/")
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_find_user_that_dont_exist(self):
+        response = self.client.get("/api/users/99999/")
 
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
@@ -69,9 +114,69 @@ class UserViewDetailTest(APITestCase):
         self.assertEqual(self.user.email, "updated@example.com")
         self.assertEqual(self.user.password, "updatedpassword")
 
+    def test_update_user_that_dont_exist(self):
+        data = {
+            "name": "updatedname",
+            "email": "updated@example.com",
+            "password": "updatedpassword",
+        }
+
+        response = self.client.put(f"/api/users/99999/", data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_update_user_with_wrong_email(self):
+        data = {
+            "name": "updatedname",
+            "email": "wrongemail",
+            "password": "updatedpassword",
+        }
+
+        response = self.client.put(f"/api/users/{self.user.id}/", data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    
+    def test_update_user_with_no_email(self):
+        data = {
+            "name": "updatedname",
+            "email": "",
+            "password": "updatedpassword",
+        }
+
+        response = self.client.put(f"/api/users/{self.user.id}/", data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_update_user_with_no_password(self):
+        data = {
+            "name": "updatedname",
+            "email": "updated@example.com",
+            "password": "",
+        }
+
+        response = self.client.put(f"/api/users/{self.user.id}/", data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    
+    def test_update_user_with_no_name(self):
+        data = {
+            "name": "",
+            "email": "updated@example.com",
+            "password": "updatedpassword",
+        }
+
+        response = self.client.put(f"/api/users/{self.user.id}/", data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
     def test_delete_user(self):
         response = self.client.delete(f"/api/users/{self.user.id}/")
 
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
         self.assertFalse(User.objects.filter(id=self.user.id).exists())
+
+    def test_delete_user_that_dont_exist(self):
+        response = self.client.delete(f"/api/users/99999/")
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
