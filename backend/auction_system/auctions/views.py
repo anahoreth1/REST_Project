715141abc302bb8d107313326ee9366ff2fd9ce3
Ochudniak <ochudniak41@gmail.com>
2026-06-18 00:10:47 +0200

    Update views.py

--- backend/auction_system/auctions/views.py
+++ backend/auction_system/auctions/views.py
@@ -2,16 +2,24 @@ from django.shortcuts import get_object_or_404
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.utils import timezone
 
 from .models import Auction
 from .serializers import AuctionSerializer, BidSerializer
 
+def close_expired_auctions():
+    Auction.objects.filter(
+        status="active",
+        end_date__lte=timezone.now()
+    ).update(status="ended")
 
 # Widok do wyświetlania listy aukcji i dodawanie nowych
 class AuctionListCreateView(generics.ListCreateAPIView):
     serializer_class = AuctionSerializer
 
     def get_queryset(self):
+        close_expired_auctions()
+
         queryset = Auction.objects.all()
 
         # Pobranie parametrów filtrowania z adresu URL
@@ -31,14 +39,40 @@ class AuctionListCreateView(generics.ListCreateAPIView):
 
 # Widok do pobierania, edycji i usuwania jednej aukcji
 class AuctionDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Auction.objects.all()
     serializer_class = AuctionSerializer
+
+    def get_queryset(self):
+        close_expired_auctions()
+
+        queryset = Auction.objects.all()
+
+        # Pobranie parametrów filtrowania z adresu URL
+        category = self.request.query_params.get("category")
+        status_param = self.request.query_params.get("status")
+
+        # Filtrowanie po kategorii
+        if category:
+            queryset = queryset.filter(category=category)
+
+        # Filtrowanie po statusie
+        if status_param:
+            queryset = queryset.filter(status=status_param)
+
+        return queryset
 
 
 # Widok do składania ofert
 class AuctionBiddingView(APIView):
     def post(self, request, auction_id):
+        close_expired_auctions()
+
         auction = get_object_or_404(Auction, id=auction_id)
+        if auction.status == "ended":
+            return Response(
+                {"error": "Ta aukcja już się zakończyła."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
         serializer = BidSerializer(data=request.data)
 
         if not serializer.is_valid():
